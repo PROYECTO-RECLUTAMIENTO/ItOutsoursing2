9cbd2f83dc1d5a246de34d5bedc52ad633ff10bc
Jairgt1 <114253667+Jairgt1@users.noreply.github.com>
2022-11-14 21:18:05 -0600

    Update Evaluaciones.php

--- Evaluaciones.php
+++ Evaluaciones.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../css/styleEvs.css">
-    <title>IT O | Inicio </title>
+    <link rel="stylesheet" href="css/styleEvs.css">
+    <link rel="stylesheet" href="css/evaluaciones.css">
+    <title>IT O | Evaluaciones </title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="LoginEvaluacion.php"><img src="../../img/LogoITO.png" style="width:4rem;"></a>
+        <a class="navbar-brand" href="LoginEvaluacion.php"><img src="img/LogoITO.png" style="width:4rem;"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -29,8 +30,34 @@
     <main>
     <br><br><br>
         <section class="vh-100" >
-           
-            
+          <h1>Evaluaciones</h1>
+          <div id="exam">
+            <div id="exam1">
+              <div id="icono">
+                  <img src="img/conocimiento.png" style="max-width:100%;width:auto;height:auto;max-height:80%;align:middle;">
+              </div>
+                 <p>Conocimiento</p>
+              <div id="txtExam">
+                <p>Evaluacion sin realizar.</p>
+              </div>
+              <div id="btnExam">
+                <p><a href="Examen.php">Realizar</a></p>
+              </div>
+            </div>
+
+            <div id="exam2">
+              <div id="icono">
+                  <img src="img/psicometrico.png" style="max-width:100%;width:auto;height:auto;max-height:80%;align:middle;">
+              </div>
+                 <p>Conocimiento</p>
+              <div id="txtExam2">
+                <p>Realizar primero el examen de conocimiento.</p>
+              </div>
+              <div id="btnExam2">
+                <p><a href="Evaluaciones.php">No disponible</a></p>
+             </div>
+
+           </div>    
         </section>
     </main>
     <!-- Footer -->
