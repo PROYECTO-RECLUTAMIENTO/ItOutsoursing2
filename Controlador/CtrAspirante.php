c9712372ace52c43f4299f37db5886242f461d5b
MauricioRomero901 <114264726+MauricioRomero901@users.noreply.github.com>
2022-11-15 19:25:48 -0600

    Controlador Aspirante
    Se agrego un archivo php donde se  realizo un metodo que consulta la BD los aspirantes a examenes

---
+++ Controlador/CtrAspirante.php
@@ -0,0 +1,40 @@
+<?php
+require 'Config/conexion.php';
+class CtrAspirante{
+
+
+
+    function fechasExamenes(){
+
+        $objeto = new Conexion("sql9.freemysqlhosting.net","sql9574012","HDHmBbJhbI","sql9574012");
+            
+        $objeto->conectarBD();
+                    
+        $sql="SELECT * FROM fechas_examen"; 	                
+        $resultado = $objeto->ejecutarQuery($sql);
+        
+        while ($reg=mysqli_fetch_array($resultado)){
+            echo'
+            <tr>
+                <td>'.$reg[0].' </td>
+                <td>'.$reg[1].' </td>
+                <td>'.$reg[2].' </td>
+                <td>'.$reg[3].' </td>
+                <td>'.$reg[4].' </td>
+                <td>'.$reg[5].' </td>
+                <td>'.$reg[6].' </td>
+                <td>'.$reg[7].' </td>
+            </tr>
+            
+            ';
+
+            $objeto ->cerrarBD();
+            return;
+        }
+        
+    }
+
+
+}
+
+?>
