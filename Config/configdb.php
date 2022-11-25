9f5b8864fe763fcc4d6c6ad92d26dfa044c9461b
omar-angeles <114208855+omar-angeles@users.noreply.github.com>
2022-11-24 23:56:07 -0600

    arreglo en conexion a base de datos

--- Config/configdb.php
+++ Config/configdb.php
@@ -1,12 +1,10 @@
 <?php
    $dbhost = "utnestudiante0325.utn.red";
-   $usuario = "utn7xtvd";
    $password = "k3n!Wt6thc4*Cq8s";
-   $dbhost = "%";
-   $usuario = "utn7xtvd_evaluaciones";
-   $password = "k5JZgIbmy!cN";
+   $usuario = "utn7xtvd";
    $dbname = "utn7xtvd_itoutsoursing";
    $conexion = new mysqli($dbhost, $usuario, $password, $dbname);
    $conexion-> set_charset("utf8");
 
 ?>
+<!-- $conn = mysqli_connect("utnestudiante0325.utn.red","utn7xtvd","k3n!Wt6thc4*Cq8s","utn7xtvd_itoutsoursing") -->
