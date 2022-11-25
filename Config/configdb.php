1e128cd82372074427e3dacf52c36addc94adf76
omar-angeles <114208855+omar-angeles@users.noreply.github.com>
2022-11-24 21:22:26 -0600

    Arreglo en mensaje raro en cofigdb.php

--- Config/configdb.php
+++ Config/configdb.php
@@ -1,13 +1,10 @@
 <?php
-<<<<<<< HEAD
    $dbhost = "utnestudiante0325.utn.red";
    $usuario = "utn7xtvd";
    $password = "k3n!Wt6thc4*Cq8s";
-=======
    $dbhost = "%";
    $usuario = "utn7xtvd_evaluaciones";
    $password = "k5JZgIbmy!cN";
->>>>>>> 5f5cc82e7c8bb57958e2f3a3da0edb240da9bd7a
    $dbname = "utn7xtvd_itoutsoursing";
    $conexion = new mysqli($dbhost, $usuario, $password, $dbname);
    $conexion-> set_charset("utf8");
