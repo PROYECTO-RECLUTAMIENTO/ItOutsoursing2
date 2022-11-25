12c483553017a77296ef49fd579ff4edb13df578
omar-angeles <114208855+omar-angeles@users.noreply.github.com>
2022-11-24 21:15:44 -0600

    arreglo en la conexion de la base de datos

--- Config/configdb.php
+++ Config/configdb.php
@@ -1,8 +1,8 @@
 <?php
-   $dbhost = "sql9.freemysqlhosting.net";
-   $usuario = "sql9574012";
-   $password = "HDHmBbJhbI";
-   $dbname = "sql9574012";
+   $dbhost = "utnestudiante0325.utn.red";
+   $usuario = "utn7xtvd";
+   $password = "k3n!Wt6thc4*Cq8s";
+   $dbname = "utn7xtvd_itoutsoursing";
    $conexion = new mysqli($dbhost, $usuario, $password, $dbname);
    $conexion-> set_charset("utf8");
 
