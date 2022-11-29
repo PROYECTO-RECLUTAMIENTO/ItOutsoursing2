a6bdaad9a328eaecf155ad58c150f66b61b520cf
Jairgt1 <114253667+Jairgt1@users.noreply.github.com>
2022-11-29 01:27:02 -0600

    Update ValidacionBtn.php

--- Controlador/ValidacionBtn.php
+++ Controlador/ValidacionBtn.php
@@ -3,8 +3,8 @@ include '../../Config/conexion.php';
 
 $folio = $_SESSION["folio"];
 
-$objeto = new Conexion("sql9.freemysqlhosting.net","sql9574012","HDHmBbJhbI","sql9574012");
-            
+$objeto = new Conexion("utnestudiante0325.utn.red","utn7xtvd","k3n!Wt6thc4*Cq8s","utn7xtvd_itoutsoursing");
+                     
 $objeto->conectarBD();
     
 $sql="SELECT F.estatus FROM examenes E,fechas_examen F WHERE F.id_aspirante = $folio AND E.id_examen = F.id_examen AND E.id_tipo = '1'"; 	                
@@ -19,14 +19,14 @@ $objeto ->cerrarBD();
 function texto1(){  
     
     $txt= "";
-if($GLOBALS['realizado'] == 0){
-    $txt= "<p>Evaluacion sin realizar.</p>
+if($GLOBALS['realizado'] == 1){
+    $txt= "<p>Evaluación sin realizar.</p>
     </div>
     <div id='btnExam' class='btn1'>
       <p><a onclick='boton1()'>Realizar</a></p>
     </div>";
 }else{
-    $txt= "<p id='EvaRea'>Evaluacion realizada.</p> 
+    $txt= "<p id='EvaRea'>Evaluación realizada.</p> 
     </div>
     <div id='btnExam' class='btn2'>
       <p><a onclick='boton1()'>No disponible</a></p>
@@ -36,7 +36,7 @@ return $txt;
 }
 
 function accionbtn1(){
-    if($GLOBALS['realizado'] == 0){
+    if($GLOBALS['realizado'] == 1){
         $accion= "window.location.href = '../../Vista/Usuario/Examen.php';";
     }else{
         $accion="Swal.fire({
@@ -51,7 +51,7 @@ return $accion;
 
 function texto2(){  
     $txt2= "";
-if($GLOBALS['realizado'] == 1){
+if($GLOBALS['realizado'] == 0){
     $txt2= "<p id='EvaRea'>El examen de conocimiento ya ha sido realizado.</p>
     </div>
     <div id='btnExam2' class='btn1'>
@@ -68,8 +68,8 @@ return $txt2;
 }
 
 function accionbtn2(){
-    if($GLOBALS['realizado'] == 1){
-        $accion2= "window.location.href = '../../Vista/Usuario/Examen.php';";
+    if($GLOBALS['realizado'] == 0){
+        $accion2= "window.location.href = '../../Vista/Usuario/Evaluaciones.php';";
     }else{
         $accion2="Swal.fire({
             title: 'No has realizado el examen de conocimiento.',
